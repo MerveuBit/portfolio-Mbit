83e47f929ccb575dc5079bffa42d95a22a9e8d79
MerveuBit <merveubitangimana73@gmail.com>
2025-06-03 13:16:53 +0200

    Modifications du portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -218,8 +218,8 @@ const testimonials = [
     imgPath: "/images/c3.png",
   },
   {
-    name: "Meker",
-    mentions: "@meker",
+    name: "Merni",
+    mentions: "@merni",
     review:
       "Collaborating with Merveu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Merveu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Merveu is the ideal partner.",
     imgPath: "/images/c5.png",
